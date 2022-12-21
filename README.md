91c28e97a90fef0b88b1b2ab4a589f82e2aa423e
David Smoker <dsmoker@gmail.com>
2022-12-21 13:05:23 +0000

    Updated docs and bumped version

--- README.md
+++ README.md
@@ -5,11 +5,14 @@ Converts infix tokens to postfix tokens, otherwise known as <https://en.wikipedi
 This library does not do parsing or execution of expressions, but only filters/reorders tokens to allow stack-based execution.
 The (possibly less efficient) alternative to stack-based execution usually involves walking an expression tree.
 
-## Examples
+## Example
 ```rust
 use fixit::{BinaryOperator, InfixToken, convert};
 
-// Define your own operator
+// Define your own operand: it could be anything.
+type MyOperand = f32;
+
+// Define your own operator.
 enum MyBinaryOp {
     Add,
     Sub,
@@ -29,16 +32,16 @@ impl BinaryOperator for MyBinaryOp {
     }
 }
 
-// Parse and cache the postfix expression
-fn parse_and_cache_expr(expr: &str) {
-    // Parsing an expression like "a + b * c + d" should give:
-    // vec![
-    //     InfixToken::Operand("a"), InfixToken::BinaryOp(MyBinaryOp::Add), InfixToken::Operand("b"),
-    //     InfixToken::BinaryOp(MyBinaryOp::Mul),
-    //     InfixToken::Operand("c"), InfixToken::BinaryOp(MyBinaryOp::Add), InfixToken::Operand("d")
-    // ]
-    let infix_tokens = parse_expr(expr); // Could be done via crates `nom`, `pest`, or others
+type MyInfixToken = InfixToken<MyOperand, MyBinaryOp>;
 
+// The incoming `expr` tokens could have been parsed from a string via a crate like `nom` or `pest`.
+// Parsing an expression like "a + b * c + d" should give:
+// vec![
+//     InfixToken::Operand("a"), InfixToken::BinaryOp(MyBinaryOp::Add), InfixToken::Operand("b"),
+//     InfixToken::BinaryOp(MyBinaryOp::Mul),
+//     InfixToken::Operand("c"), InfixToken::BinaryOp(MyBinaryOp::Add), InfixToken::Operand("d")
+// ]
+fn example(expr: Vec<MyInfixToken>) {
     // Convert to postfix. The result will contain:
     // vec![
     //     PostfixToken::Operand("a"),
@@ -49,9 +52,9 @@ fn parse_and_cache_expr(expr: &str) {
     //     PostfixToken::Operand("d"),
     //     PostfixToken::BinaryOp(MyBinaryOp::Add)
     // ]
-    let postfix_tokens = convert(infix_tokens);
+    let postfix_tokens = convert(expr);
 
-    // We don't aim to provide a full explanation of postfix expression here, but
+    // We don't aim to provide a full explanation of postfix expressions here, but
     // the expression can now be evaluated by iterating over postfix tokens,
     // pushing and popping values off a stack.
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,14 @@
 //! This library does not do parsing or execution of expressions, but only filters/reorders tokens to allow stack-based execution.
 //! The (possibly less efficient) alternative to stack-based execution usually involves walking an expression tree.
 //!
-//! # Examples
+//! # Example
 //! ```
 //! use fixit::{BinaryOperator, InfixToken, convert};
 //!
-//! // Define your own operator
+//! // Define your own operand: it could be anything.
+//! type MyOperand = f32;
+//!
+//! // Define your own operator.
 //! enum MyBinaryOp {
 //!     Add,
 //!     Sub,
@@ -28,16 +31,16 @@
 //!     }
 //! }
 //!
-//! // Parse and cache the postfix expression
-//! fn parse_and_cache_expr(expr: &str) {
-//!     // Parsing an expression like "a + b * c + d" should give:
-//!     // vec![
-//!     //     InfixToken::Operand("a"), InfixToken::BinaryOp(MyBinaryOp::Add), InfixToken::Operand("b"),
-//!     //     InfixToken::BinaryOp(MyBinaryOp::Mul),
-//!     //     InfixToken::Operand("c"), InfixToken::BinaryOp(MyBinaryOp::Add), InfixToken::Operand("d")
-//!     // ]
-//!     let infix_tokens = parse_expr(expr); // Could be done via crates `nom`, `pest`, or others
+//! type MyInfixToken = InfixToken<MyOperand, MyBinaryOp>;
 //!
+//! // The incoming `expr` tokens could have been parsed from a string via a crate like `nom` or `pest`.
+//! // Parsing an expression like "a + b * c + d" should give:
+//! // vec![
+//! //     InfixToken::Operand("a"), InfixToken::BinaryOp(MyBinaryOp::Add), InfixToken::Operand("b"),
+//! //     InfixToken::BinaryOp(MyBinaryOp::Mul),
+//! //     InfixToken::Operand("c"), InfixToken::BinaryOp(MyBinaryOp::Add), InfixToken::Operand("d")
+//! // ]
+//! fn example(expr: Vec<MyInfixToken>) {
 //!     // Convert to postfix. The result will contain:
 //!     // vec![
 //!     //     PostfixToken::Operand("a"),
@@ -48,9 +51,9 @@
 //!     //     PostfixToken::Operand("d"),
 //!     //     PostfixToken::BinaryOp(MyBinaryOp::Add)
 //!     // ]
-//!     let postfix_tokens = convert(infix_tokens);
+//!     let postfix_tokens = convert(expr);
 //!
-//!     // We don't aim to provide a full explanation of postfix expression here, but
+//!     // We don't aim to provide a full explanation of postfix expressions here, but
 //!     // the expression can now be evaluated by iterating over postfix tokens,
 //!     // pushing and popping values off a stack.
 //! }
